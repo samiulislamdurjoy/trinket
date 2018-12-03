87b5342d80127f894f410c3cab5395ae97fb2381
Samiul Islam <samiulislamdurjoy@gmail.com>
2018-12-03 07:29:02 +0600

    Create expected_number_counter_from_list.py

---
+++ expected_number_counter_from_list.py
@@ -0,0 +1,7 @@
+def count_num(num_for_count_num):
+  count_for_num_for_count_num = 0
+  for get_value in num_for_count_num:
+    if get_value == 6:
+      count_for_num_for_count_num += 1
+  return count_for_num_for_count_num
+print(count_num([5,6,7,6,5,8,6,5,6,5,6,5,6]))
